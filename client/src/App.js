a743ff7350a1642fa7cb97d5ea5634b60a6d77d4
Deneth Pinsara <it21158186@my.sliit.lk>
2024-01-17 23:04:31 +0530

    set a new header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Landing from '../pages/landing/Landing';
-import About from '../pages/about/About';
-import Contact from '../pages/contact/Contact';
-import Portfolio from '../pages/portfolio/Portfolio';
+import Landing from './pages/landing/Landing';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Portfolio from './pages/portfolio/Portfolio';
+import Navbar from './components/PageHeader';
 
 
 function App() {
   return (
+    
     <Router>
+      <Navbar />
       <div className="App">
         <Switch>
           <Route path="/landing" component={Landing} />
