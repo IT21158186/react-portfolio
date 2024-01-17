489f871761edd501b5ddddfe9c3063e5b40781cd
Deneth Pinsara <it21158186@my.sliit.lk>
2024-01-17 22:45:33 +0530

    all update with a proper frontend

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from '../pages/landing/Landing';
+import About from '../pages/about/About';
+import Contact from '../pages/contact/Contact';
+import Portfolio from '../pages/portfolio/Portfolio';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/landing" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/portfolio" component={Portfolio} />
+        </Switch>
+      </div>
+    </Router>
+  );
+
+}
+
+export default App;
