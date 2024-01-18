71609e7f47192e293d3b184afb1edcd2fd321829
Deneth Pinsara <it21158186@my.sliit.lk>
2024-01-19 01:23:05 +0530

    app.js update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/landing/Landing';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
@@ -13,12 +13,12 @@ function App() {
     <Router>
       <Navbar />
       <div className="App">
-        <Switch>
-          <Route path="/landing" component={Landing} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/portfolio" component={Portfolio} />
-        </Switch>
+        <Route>
+          <Route path="/landing" element={Landing} />
+          <Route path="/about" element={About} />
+          <Route path="/contact" element={Contact} />
+          <Route path="/portfolio" element={Portfolio} />
+        </Route>
       </div>
     </Router>
   );
